563e525a98505db15203c3746c3ff7a3c9037d3c
priyanshu8081 <pc0749711189@gmail.com>
2026-04-13 21:00:27 +0530

    second changes

--- frontend/src/components/Courses.jsx
+++ frontend/src/components/Courses.jsx
@@ -8,11 +8,11 @@ const Courses = () => {
         <div className='mt-28 justify-center items-center text-center'>
           <h1 className='text-2xl md:text-4xl'>We proudly say that we stand <span className='text-[#00A385]'>with the students..</span> </h1>
           <p className='mt-6 md:mt-12'>Every expert developer once struggled with their first line of code and every mistake became a step toward mastery.We make that journey simpler   and proudly stand <span className='text-[#00A385]'>with students learning without fear..</span></p>
-          <button className='btn mt-6 bg-[#00A385] text-white'><Link to={'/'} >Back to Home</Link></button>
+          <Link className="btn mt-6 bg-[#00A385] text-white" to={'/'} >Back to Home</Link>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {
-            list.map((items)=>(
+            list.map((items) => (
               <Card key={items.id} items={items} />
             ))
           }
--- frontend/src/components/FreeBook.jsx
+++ frontend/src/components/FreeBook.jsx
@@ -20,8 +20,8 @@ const FreeBook = () => {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true
                 }
@@ -29,18 +29,19 @@ const FreeBook = () => {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                     initialSlide: 2
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
+                    slidesToShow: 1, // 👈 for 430px
+                    slidesToScroll: 1,
+                    arrows: false,
+                },
+            },
         ]
     };
 
